cc4f666689e43f2370330b6ecda2cec58b114de0
Govarthanan D <116496049+GovarthananD@users.noreply.github.com>
2024-01-07 17:06:57 +0530

    Add files via upload

---
+++ task6/alert.js
@@ -0,0 +1,16 @@
+alert("Hi Hello");
+alert("3+1 is 4 are you sure?");
+
+let admin = 9,
+  fname = 10.5;
+fname = "Guvi";
+lname = "geek";
+admin = fname + lname;
+alert(admin);
+
+var a = "2" > "12";
+if (a) {
+  alert("Code is Blasted");
+} else {
+  alert("Diffused");
+}
